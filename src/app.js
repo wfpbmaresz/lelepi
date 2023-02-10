84d20f8e2bb69bbb94b6321f38dbc2dee9789372
EszterSzollosiMaruzs <maruzseszti@freemail.hu>
2023-02-10 23:45:23 +0100

    var/const elmélkedés

--- src/app.js
+++ src/app.js
@@ -7,3 +7,27 @@ Date: 2023-02-09
 Github: https://github.com/wfpbmaresz
 Licenc: GNU GPL
 */
+
+
+const calcButton = document.querySelector("#calcButton");
+const heightA = document.querySelector("#heightA");
+const heightB = document.querySelector("#heightB");
+const heightC = document.querySelector("#heightC");
+const area = document.querySelector("#area");
+
+// var vagy const????
+//A változók csak egyszer kerülnek inicializálásra, és értékük nem változik, ezért a const megfelelőbb opció lehet.
+//A var a változó értékének módosítását is lehetővé teszi, amire nincs szükség. Az adatok változása esetén új paralelepipedont kell számítani, 
+//így a változók értékét nem kell módosítani.
+//A const a kód javíthatóságát, a hibakeresést, és az átláthatóságot is elősegíti.
+
+calcButton.addEventListener("click", () => {
+  const a = parseFloat(heightA.value);
+  const b = parseFloat(heightB.value);
+  const c = parseFloat(heightC.value);
+  const volume = a * b * c;
+  area.value = volume;
+});
+
+
+console.log("Szőllősi-Maruzs Eszter, Szoft 1/I-E, 2023.02.09.")
